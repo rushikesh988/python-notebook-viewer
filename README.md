5db1f0a635c2f15c964d8b380d3b58da27d8a9fc
Rushikesh Tade <rushtade@in.ibm.com>
2019-03-07 19:45:41 +0530

    Updated readme and added comments to renderer.js

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 # Python Notebook Viewer
 Help view pynb files in browser without running jupyter notebook server. 
 
+## File Structure
 
-
+ 1. **renderer.js** : Contains code to fetch text from current python notebook file opened in firefox and convert it into HTML using notebook.min.js
+ 2. **js/notebook.min.js**: Notebook.JS library to convert Jupyter Notebook files into HTML code. [Official Repository](https://github.com/jsvine/notebookjs)
+ 3. **js/prism.min.js**: One of the dependencies of Notebook JS, Prism is a lightweight, extensible syntax highlighter. Available at: [Official Repository](https://github.com/PrismJS/prism)   , [Website](https://prismjs.com/#languages)
+ 4. **js/marked.min.js**: Dependency of Notebook JS, Marked JS is a markdown parser and compiler. [Offical Site](https://marked.js.org/#/README.md#README.md), [Repository](https://github.com/markedjs/marked)
+ 5. **js/katex-auto-render.min.js**: Dependency of Notebook JS , KaTeX is a math typesetting library for the web.[Offical Site](https://katex.org/), [Repository](https://github.com/KaTeX/KaTeX)
+ 6. **js/katex.min.js**: Same as Above
+ 7. **js/es5-shim.min.js** : Dependency of Notebook JS .  [Repository](https://github.com/es-shims/es5-shim)
+ 8. **js/ansi_up.min.js** : Notebook JS dependency . Used to convert\ ansi sequences in strings to colorful HTML
+ 9. **icons/***: Directory containing all the icon images.
+ 10. **css/***: Directory containing CSS files required by Notebook JS and its dependency libraries.
+ 11. **manifest.json**: Manifest file for Firefox containing all the information about this plugin.
+ 
 
 ## Licence 
-This code is released under Mozilla Public Licence.  There are multiple JS as CSS files included in this repository each with different licence terms. It is advised to go through them before using this any purpose. 
-
+This code is released under Mozilla Public License.  There are multiple JS as CSS files included in this repository each with different license terms. It is advised to go through them before using for any purpose. 
 
 
 
--- renderer.js
+++ renderer.js
@@ -1,45 +1,58 @@
-var notebookJSON=JSON.parse(document.body.textContent)
-var notebook = nb.parse(notebookJSON);
-var rendered = notebook.render();
-document.body.textContent=""
+var notebookJSON=JSON.parse(document.body.textContent) // read document content and convert to JSON
+var notebook = nb.parse(notebookJSON); // parse it to Notebook.js format
+var rendered = notebook.render(); // render it into HTML . rendered is a string containing html 
+
+// remove contents from current page/tab
+document.body.textContent="" 
 document.body.innerHTML=""
 document.body.outerHTML=""
 
+//add plugin logo image to top . This is done by adding image to DOM 
 var logoImage=document.createElement("img")
 logoImage.src=browser.extension.getURL("icons/icon256.png");
 logoImage.style="height:48px;width:48px"
 
+//add plugin name after image . This is done by adding text to DOM 
 var headText=document.createElement("text")
 headText.innerText="   Python Notebook Viewer"
 headText.style="font-family:Consolas, Monaco, 'Andale Mono', monospace;font-size:48px;"
 
+//add vertical line below plugin name . This is done by adding image to DOM 
 var topHr=document.createElement("HR")
 topHr.style="width:98%;"
 
+// declare container space for above elements
 var pluginHead=document.createElement("div")
 pluginHead.id="pluginHead"
 pluginHead.style="width: 85%;margin: auto;background:#ffffff;"
 
+//put all the elements declared above in a container
 pluginHead.appendChild(logoImage)
 pluginHead.appendChild(headText)
 
+//initiate space in page to put HTML of Jupyter Notebook.
 var notebookCodeHolder=document.createElement("div")
 notebookCodeHolder.id="notebook-code-holder"
 notebookCodeHolder.style="width: 85%;margin: auto;background:#ffffff;"//"width: 80%;margin: auto;background:#ffffff;"
 notebookCodeHolder.appendChild(rendered)
 
+// Declare outer space to for HTML , this done for color adjustment. 
 var notebookHolder=document.createElement("div")
 notebookHolder.id="notebook-holder"
 notebookHolder.style="margin: auto;background:#ffffff;"//"width: 90%;margin: auto;background:#ffffff;"
 notebookHolder.appendChild(notebookCodeHolder)
 
+// create a horizontal line 
 var bottomHr=document.createElement("HR")
 bottomHr.style="width:98%;"
 
+//create main page holder for the for color arrangement
 var pageHolder=document.createElement("div")
 pageHolder.id="page-holder"
 pageHolder.style="width: 78%;margin: auto;background:#ffffff;"
 
+
+// add components by sequence.
 pageHolder.appendChild(pluginHead)
 pageHolder.appendChild(topHr)
 pageHolder.appendChild(notebookHolder)
@@ -48,5 +61,7 @@ pageHolder.appendChild(bottomHr)
 document.body.appendChild(pageHolder)
 
 document.body.style="background:#f5f2f0;"
+
+//call prism for Highlighting of code
 Prism.highlightAll();
 
